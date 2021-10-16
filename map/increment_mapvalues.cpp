4cf2b750807889b75603e864e525ffaae47e2a67
Raavi Venkata Sai Chaitanya <69502127+chaitanya-191855@users.noreply.github.com>
2021-10-16 14:17:50 +0530

    Add files via upload

---
+++ map/increment_mapvalues.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int> arr{1,2,3,4,1,2,1,2,2,4,5,6,1,2,3,4};
+    map<int,int> m;
+    for(int i=0;i<arr.size();i++){
+        auto it=m.find(arr[i]);
+        if(m.empty()){
+            m.insert({arr[i],1});
+        }else if(it==m.end()){
+            m.insert({arr[i],1});
+        }else{
+            it->second++;
+        }
+    }
+    for(auto it=m.begin();it!=m.end();it++){
+        cout<<it->first<<" "<<it->second<<"\n";
+    }
+}
