5ed7cb9e3b690966d505d3239deda7062e060155
Raavi Venkata Sai Chaitanya <69502127+chaitanya-191855@users.noreply.github.com>
2021-10-25 21:12:23 +0530

    Update Get min at pop.cpp

--- stack/Get min at pop.cpp
+++ stack/Get min at pop.cpp
@@ -32,3 +32,19 @@ void _getMinAtPop(stack<int>s)
         cout<<ve[i]<<" ";
     }
 }
+
+example::
+Input:
+N = 5
+A = {1 2 3 4 5}
+Output: 
+1 1 1 1 1
+Explanation: 
+After pushing elements to the stack, 
+the stack will be "top -> 5, 4, 3, 2, 1". 
+Now, start popping elements from the stack
+popping 5: min in the stack is 1.popped 5
+popping 4: min in the stack is 1. popped 4
+popping 3: min in the stack is 1. popped 3
+popping 2: min in the stack is 1. popped 2
+popping 1: min in the stack is 1. popped 1
