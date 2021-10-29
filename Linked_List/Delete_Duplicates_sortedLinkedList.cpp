45dfa7536105ea8b21b2483a0269357aa88bc709
Raavi Venkata Sai Chaitanya <69502127+chaitanya-191855@users.noreply.github.com>
2021-10-29 10:30:07 +0530

    Add files via upload

---
+++ Linked_List/Delete_Duplicates_sortedLinkedList.cpp
@@ -0,0 +1,23 @@
+void removeDuplicates(Node* head)
+{
+    Node* current = head;
+ 
+    Node* next_next;
+     
+    if (current == NULL)
+    return;
+ 
+    while (current->next != NULL)
+    {
+    if (current->data == current->next->data)
+    { 
+        next_next = current->next->next;
+        free(current->next);
+        current->next = next_next;
+    }
+    else
+    {
+        current = current->next;
+    }
+    }
+}
